035f0d22f5ea98837c9e7f417c992066cd8d5108
gkrhines <gkrhines21@ou.edu>
2020-12-10 22:20:19 -0600

    Initial Commit

---
+++ R/myF.R
@@ -0,0 +1,15 @@
+#' Test Function
+#'
+#' Description
+#'
+#' @param x a vector
+#'
+#' @return a vector of squared components
+#' @export
+#'
+#' @examples
+#' x <- 1:30; myF(x)
+myF <- function(x)
+{
+  return(x^2)
+}
